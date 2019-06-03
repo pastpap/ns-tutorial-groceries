994c809df68616be2b58b3b57513663dc4749e08
nipap <stefan.papin@gmail.com>
2019-06-03 23:59:51 +0200

    route to page

---
+++ src/app/pages/list/list.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "list",
+  templateUrl: "list.component.html",
+  styleUrls: ["list-common.css", "list.css"]
+})
+export class ListComponent {}
