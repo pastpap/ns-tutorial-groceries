d7a3c9ed24667cb83b84e8820d561bc17fa73146
nipap <stefan.papin@gmail.com>
2019-06-06 02:21:08 +0200

    class finished

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,8 +1,91 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  NgZone
+} from "@angular/core";
+import { Grocery } from "../../shared/grocery/grocery";
+import { GroceryListService } from "../../shared/grocery/grocery-list.service";
+import { TextField } from "tns-core-modules/ui/text-field";
+
+import * as SocialShare from "nativescript-social-share";
 
 @Component({
   selector: "list",
+  providers: [GroceryListService],
   templateUrl: "list.component.html",
   styleUrls: ["list-common.css", "list.css"]
 })
-export class ListComponent {}
+export class ListComponent implements OnInit {
+  public groceryList: Grocery[] = [];
+  public grocery = "";
+  public isLoading = false;
+  public listLoaded = false;
+  @ViewChild("groceryTextField") groceryTextField: ElementRef;
+
+  constructor(
+    private groceryListService: GroceryListService,
+    private zone: NgZone
+  ) {}
+
+  public ngOnInit() {
+    this.isLoading = true;
+    this.groceryListService.load().subscribe(
+      loadedGroceries => {
+        loadedGroceries.forEach(groceryObject => {
+          this.groceryList.unshift(groceryObject);
+        });
+        this.isLoading = false;
+        this.listLoaded = true;
+      },
+      error => {
+        this.isLoading = false;
+      }
+    );
+  }
+  public add() {
+    this.isLoading = true;
+    if (this.grocery.trim() === "") {
+      alert("Enter a grocery item");
+      return;
+    }
+
+    // Dismiss the keyboard
+    let textField = <TextField>this.groceryTextField.nativeElement;
+    textField.dismissSoftInput();
+
+    this.groceryListService.add(this.grocery).subscribe(
+      groceryObject => {
+        this.groceryList.unshift(groceryObject);
+        this.grocery = "";
+        this.isLoading = false;
+      },
+      () => {
+        alert({
+          message: "And error occurred while adding an item to your list.",
+          okButtonText: "OK"
+        });
+        // this.grocery = "";
+        this.isLoading = false;
+      }
+    );
+  }
+  delete(grocery: Grocery) {
+    this.groceryListService.delete(grocery.id).subscribe(() => {
+      // Running the array splice in a zone ensures that change detection gets triggered.
+      this.zone.run(() => {
+        let index = this.groceryList.indexOf(grocery);
+        this.groceryList.splice(index, 1);
+      });
+    });
+  }
+  public share() {
+    const list = [];
+    for (let i = 0, size = this.groceryList.length; i < size; i++) {
+      list.push(this.groceryList[i].name);
+    }
+    const listString = list.join(", ").trim();
+    SocialShare.shareText(listString);
+  }
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
+
+import { Page } from "tns-core-modules/ui/page";
+import { Color } from "tns-core-modules/color";
+import { View } from "tns-core-modules/ui/core/view";
+
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
+import { setHintColor } from "../../utils/hint-util";
+import { TextField } from "tns-core-modules/ui/text-field";
 
 @Component({
   selector: "gr-main",
@@ -9,16 +16,31 @@ import { UserService } from "../../shared/user/user.service";
   templateUrl: "login.component.html",
   styleUrls: ["login-common.css", "login.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public user: User;
   public isLoggingIn = true;
+  @ViewChild("container") container: ElementRef;
+  @ViewChild("email") email: ElementRef;
+  @ViewChild("password") password: ElementRef;
 
-  constructor(private router: Router, private userService: UserService) {
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private page: Page
+  ) {
     this.user = new User();
     this.user.email = "sp@me.com";
     this.user.password = "admin123";
   }
+  ngOnInit(): void {
+    this.page.actionBarHidden = true;
+    this.page.backgroundImage = "res://bg_login";
+  }
   public submit() {
+    if (!this.user.isValidEmail()) {
+      alert("Email invalid!");
+      return;
+    }
     if (this.isLoggingIn) {
       this.login();
     } else {
@@ -44,5 +66,25 @@ export class LoginComponent {
   }
   public toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
+    this.setTextFieldColors();
+    const container = <View>this.container.nativeElement;
+    container.animate({
+      backgroundColor: this.isLoggingIn
+        ? new Color("white")
+        : new Color("#301217"),
+      duration: 200
+    });
+  }
+  private setTextFieldColors() {
+    const emailTextField = <TextField>this.email.nativeElement;
+    const passwordTextField = <TextField>this.password.nativeElement;
+
+    const mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+    emailTextField.color = mainTextColor;
+    passwordTextField.color = mainTextColor;
+
+    const hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+    setHintColor({ view: emailTextField, color: hintColor });
+    setHintColor({ view: passwordTextField, color: hintColor });
   }
 }
